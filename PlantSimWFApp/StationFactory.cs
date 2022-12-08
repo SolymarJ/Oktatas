908c48141c3a32c150cd19c75d88427ce3102488
Ant1venom <botadanieltamas@gmail.com>
2022-12-08 11:17:22 +0100

    valami 2

--- PlantSimWFApp/StationFactory.cs
+++ PlantSimWFApp/StationFactory.cs
@@ -5,5 +5,6 @@ public class StationFactory
     public static Station CreateStation(int x, int y, int width, int height, string name, LinkedListOfStations LLOS, Form form, Form logForm)
     {
         return new Station(x, y, width, height, name, LLOS, form, logForm);
+        // this is some bullshit
     }
 }
